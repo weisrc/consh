6bedeca4c38acf9105d5f1931ac116a1edf95c63
Wei <46848247+weisrc@users.noreply.github.com>
2026-01-05 01:24:38 -0500

    minor test refactor

--- consh_test.go
+++ consh_test.go
@@ -30,27 +30,27 @@ func TestRemove(t *testing.T) {
 	}
 }
 
-func TestMapAllocate(t *testing.T) {
+func TestMapAllocateKeys(t *testing.T) {
 	consh := New(1.5, fnv.New64())
 	consh.Add("node1", 3)
 
-	hashes := make([]uint64, 10)
-	for i := range hashes {
-		hashes[i] = uint64(i * 100)
+	keys := make([][]byte, 10)
+	for i := 0; i < 10; i++ {
+		keys[i] = []byte{byte(i)}
 	}
 
-	assignments := consh.MapAllocateHashes(hashes)
-	if len(assignments) != 10 {
-		t.Errorf("expected 10 assignments, got %d", len(assignments))
+	allocations := consh.MapAllocateKeys(keys)
+	if len(allocations) != 10 {
+		t.Errorf("expected 10 assignments, got %d", len(allocations))
 	}
-	for _, node := range assignments {
+	for _, node := range allocations {
 		if node.Key != "node1" {
 			t.Errorf("expected all assignments to be 'node1', got '%s'", node.Key)
 		}
 	}
 	consh.Remove("node1")
-	assignments = consh.MapAllocateHashes(hashes)
-	for _, node := range assignments {
+	allocations = consh.MapAllocateKeys(keys)
+	for _, node := range allocations {
 		if node != nil {
 			t.Errorf("expected all assignments to be nil, got '%s'", node.Key)
 		}
